3dbe5120310aed4fa23efea367629ed155406099
Adnen rebai <adnen.rebai@gmail.com>
2017-12-26 15:53:47 +0100

    Create signature.js

---
+++ signature.js
@@ -0,0 +1,6 @@
+(function(){
+var mytwLink = "<a class='follow' href='https://twitter.com/adnen_rebai' target='_blank' > follow me </a>";
+var node = document.createElement("div");              
+    node.innerHTML =mytwLink;               
+document.getElementById("novis-app").appendChild(node);
+})();
